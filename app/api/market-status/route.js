cb14556a3a93ee546fc26e11f8d6fbdafb1f30c2
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-05-16 14:15:49 +0530

    Update API requests to disable caching

--- app/api/market-status/route.js
+++ app/api/market-status/route.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const response = await axios.get('https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo');
+  const response = await axios.get('https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo',  {
+    caches: "no-store",
+  });
   
   return NextResponse.json(response.data.markets);
 }
