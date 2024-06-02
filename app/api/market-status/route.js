0e564746b1487b2fb2febc23002202816850fb08
somanshubh <89822689+somanshubh@users.noreply.github.com>
2024-06-02 12:36:43 +0530

    Disable caching for API requests in market-status and marketnews

--- app/api/market-status/route.js
+++ app/api/market-status/route.js
@@ -2,10 +2,11 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
+import { unstable_noStore as nostore } from "next/cache";
 export async function GET() {
-  const response = await axios.get('https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo',  {
-    caches: "no-store",
-  });
+  nostore();
+
+  const response = await axios.get('https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=demo');
   
   return NextResponse.json(response.data.markets);
 }
--- app/marketnews/finnhub.js
+++ app/marketnews/finnhub.js
@@ -1,16 +1,16 @@
 import axios from "axios";
+
+import { unstable_noStore as nostore } from "next/cache";
 const API_KEY = process.env.FINNHUB_API_KEY;
-export const fetchMarketNews = async () => {
+export const fetchMarketNews = async () => {    nostore();
   try {
+
     const response = await axios.get(
       `https://finnhub.io/api/v1/news?category=general`,
       {
         params: {
           token: API_KEY,
         },
-      },
-      {
-        caches: "no-store",
       }
     );
     return response.data;
